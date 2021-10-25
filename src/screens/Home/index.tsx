633d4b0e9292c6ddea00ed82ec75c9842a370a03
AmauriLima <amauri.plimaj@gmail.com>
2021-10-25 14:00:54 -0300

    app.json name

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { KeyboardAvoidingView, Platform } from 'react-native';
+import React from 'react';
 
 import Header from '../../components/Header';
 import MessageList from '../../components/MessageList';
